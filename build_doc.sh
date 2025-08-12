f5bef1ef8c3d94b6182a3f951c6ae4eb429e3eaf
Alon Lanyado <alonyado@gmail.com>
2025-08-12 15:39:41 +0300

    build script

---
+++ build_doc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+FULL_CURR_PATH=$(realpath ${0%/*})
+FLD_NAME=${FULL_CURR_PATH##*/}
+OUTPUT_DIR=$(realpath ${0%/*}/../Wiki)
+
+mkdir -p ${OUTPUT_DIR}
+
+cd ${OUTPUT_DIR}
+
+ln -s -f "${FULL_CURR_PATH}/mkdocs.yml" "${OUTPUT_DIR}"
+if [ ! -d ${OUTPUT_DIR}/docs ]; then
+    ln -s "${FULL_CURR_PATH}" "${OUTPUT_DIR}/docs"
+fi
+
+# Build mkdocs
+# sudo apt install mkdocs mkdocs-material -y
+mkdocs build
+
+# Test local
+echo "To Test Locally:"
+echo  "python -m http.server -d ${OUTPUT_DIR}/site 8000"
